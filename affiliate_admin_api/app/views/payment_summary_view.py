d73f9e13633892146bef2c97e74d272d4dc18dbf
Zaman <zaman@sgcsoft.net>
2022-08-14 11:36:53 +0600

    [CP2-2119] payment summary API in affiliate-admin-api

---
+++ affiliate_admin_api/app/views/payment_summary_view.py
@@ -0,0 +1,24 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK,  HTTP_400_BAD_REQUEST
+
+from campuslibs.enrollment.common import Common
+from campuslibs.shared_utils.data_decorators import ViewDataMixin
+from campuslibs.shared_utils.shared_function import PaginatorMixin, SharedMixin
+
+
+class PaymentSummaryView(APIView, SharedMixin, ViewDataMixin):
+    http_method_names = ["head", "post"]
+
+    def post(self, request, *args, **kwargs):
+        summary = Common()
+        status, message, data = summary.payment_summary(request)
+        if not status:
+            return Response(
+                {
+                    "error": message,
+                    "status_code": 404,
+                },
+                status= HTTP_400_BAD_REQUEST,
+            )
+        return Response(self.object_decorator(data), status=HTTP_200_OK)
