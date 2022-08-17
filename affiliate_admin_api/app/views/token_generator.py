7cf2b2c8c8ca8c6ccdb28f4255893777764451de
Zaman <zaman@sgcsoft.net>
2022-08-17 14:55:44 +0600

    latest

--- affiliate_admin_api/app/views/token_generator.py
+++ affiliate_admin_api/app/views/token_generator.py
@@ -3,9 +3,13 @@ from rest_framework import exceptions
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from shared_models.models import Store, CourseProvider
+
 from django.contrib.auth.models import update_last_login
 from rest_framework_simplejwt.settings import api_settings
 
+from shared_models.models import Permission, CustomRole
+from app.serializers import CustomRoleSerializer, PermissionSerializer
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -26,6 +30,65 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         data['refresh'] = str(refresh)
         data['access'] = str(refresh.access_token)
+        data['role'] = {}
+        data['permissions'] = {}
+        data['menu_permissions'] = {}
+        data['context'] = {}
+        data['mfa_enabled'] = self.user.mfa_enabled
+
+        custom_roles = CustomRole.objects.filter(id__in=self.user.custom_roles)
+        custom_role_serializer = CustomRoleSerializer(custom_roles, many=True)
+        data['custom_roles'] = custom_role_serializer.data
+
+        permission_ids = []
+        menu_permissions = []
+        for role in custom_roles:
+            permission_ids = permission_ids + role.permissions
+            if role.menu_permissions is not None:
+                menu_permissions = menu_permissions + role.menu_permissions
+        permission_serializer = PermissionSerializer(Permission.objects.filter(id__in=permission_ids), many=True)
+        data['permissions'] = permission_serializer.data
+        data['menu_permissions'] = list(set(menu_permissions))
+        data['is_superuser'] = self.user.is_superuser
+
+        if self.user.db_context:
+            context = {}
+            if 'Store' in self.user.db_context.keys() and len(self.user.db_context['Store']):
+                context = {
+                    'type': 'Store',
+                    'values': []
+                }
+                stores = self.user.db_context['Store']
+                for store in stores:
+                    try:
+                        s = Store.objects.get(pk=store)
+                    except Store.DoesNotExist:
+                        pass
+                    else:
+                        context['values'].append({
+                            "id": s.id,
+                            "name": s.name
+                        })
+
+            if 'CourseProvider' in self.user.db_context.keys() and len(self.user.db_context['CourseProvider']):
+                context = {
+                    'type': 'CourseProvider',
+                    'values': []
+                }
+
+                course_providers = self.user.db_context['CourseProvider']
+                for cp in course_providers:
+                    try:
+                        c = CourseProvider.objects.get(pk=cp)
+                    except CourseProvider.DoesNotExist:
+                        pass
+                    else:
+                        context['values'].append({
+                            'id': c.id,
+                            'name': c.name
+                        })
+
+            data['context'] = context
 
         if api_settings.UPDATE_LAST_LOGIN:
             update_last_login(None, self.user)
